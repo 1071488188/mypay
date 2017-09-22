111e67a46d1776d7ed5f14ad7a8131bcd904a2e9
jiangjj <jiangjunjie@huianrong.com>
2017-09-22 16:17:16 +0800

    修改部分返回字段

--- src/main/java/com/har/unmanned/mfront/model/extend/ShopCommissionExtend.java
+++ src/main/java/com/har/unmanned/mfront/model/extend/ShopCommissionExtend.java
@@ -1,6 +1,7 @@
 package com.har.unmanned.mfront.model.extend;
 
 import com.har.unmanned.mfront.model.ShopCommission;
+import com.har.unmanned.mfront.utils.CheckUtil;
 import lombok.Data;
 
 /**
@@ -12,4 +13,28 @@ public class ShopCommissionExtend extends ShopCommission {
      * 时间
      */
     private String time;
+    private String amountZh;
+    private String commissionZh;
+
+    public String getAmountZh() {
+        if(!CheckUtil.isNull(amountZh)){
+          return  CheckUtil.m2(Double.parseDouble(amountZh)) ;
+        }
+        return amountZh;
+    }
+
+    public void setAmountZh(String amountZh) {
+        this.amountZh = amountZh;
+    }
+
+    public String getCommissionZh() {
+        if(!CheckUtil.isNull(commissionZh)){
+            return  CheckUtil.m2(Double.parseDouble(commissionZh)) ;
+        }
+        return commissionZh;
+    }
+
+    public void setCommissionZh(String commissionZh) {
+        this.commissionZh = commissionZh;
+    }
 }
--- src/main/java/com/har/unmanned/mfront/model/extend/ShopOrderExtend.java
+++ src/main/java/com/har/unmanned/mfront/model/extend/ShopOrderExtend.java
@@ -1,6 +1,7 @@
 package com.har.unmanned.mfront.model.extend;
 
 import com.har.unmanned.mfront.model.ShopOrder;
+import com.har.unmanned.mfront.utils.CheckUtil;
 import lombok.Data;
 
 /**
@@ -10,4 +11,16 @@ import lombok.Data;
 @Data
 public class ShopOrderExtend extends ShopOrder {
     private String headimgUrl;
+    private String amountZh;
+
+    public String getAmountZh() {
+        if(!CheckUtil.isNull(amountZh)){
+            return  CheckUtil.m2(Double.parseDouble(amountZh)) ;
+        }
+        return amountZh;
+    }
+
+    public void setAmountZh(String amountZh) {
+        this.amountZh = amountZh;
+    }
 }
